ab8ce0968e4f3874e7265c6a37f604fcefda453f
Alex <1989alex1989@gmail.com>
2020-05-14 21:08:12 +0800

    fix

--- src/pages/Review.vue
+++ src/pages/Review.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    <h2>Nice to meet you in our restaurant</h2>
-    <p>Please make a review for our restaurant and take your free coffee</p>
-    <router-link :to="{ path: '/Voucher' }" exact tag="li" active-class="active"
-      >Profile</router-link
-    >
-    <button class="btn">
-      Make review now!
-    </button>
+    <div class="wrapper">
+      <main>
+        <div class="bg">
+          <div class="container q-pt-xl">
+            <h2>Nice to meet you in our restaurant</h2>
+            <p>
+              Please make a review for our restaurant and take your free coffee
+            </p>
+            <router-link
+              :to="{ path: '/voucher' }"
+              exact
+              tag="li"
+              active-class="active"
+            >
+              <button class="btn">
+                Make review now!
+              </button></router-link
+            >
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
--- src/pages/Voucher.vue
+++ src/pages/Voucher.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <h2>Thank you for rating our restaurant!</h2>
-    <p>Show this message to your waiter and get your free coffee!</p>
+    <div class="wrapper">
+      <main>
+        <div class="bg">
+          <div class="container q-pt-xl">
+            <h2>Thank you for rating our restaurant!</h2>
+            <p>Show this message to your waiter and get your free coffee!</p>
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
