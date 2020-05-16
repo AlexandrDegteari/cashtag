bf86aaa3f5ef31dbfbf338937dc28eb382f6666f
Alex <1989alex1989@gmail.com>
2020-05-16 19:40:29 +0800

    added Restaurants table and qrcode modal

--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -2,56 +2,64 @@
   <div class="wrapper">
     <main>
       <div class="bg">
-        <div class="container q-pt-xl ">
-          <div class="q-pa-md">
-            <q-table
-              class="my-sticky-header-table"
-              title="Treats"
-              :data="data"
-              :columns="columns"
-              row-key="name"
-              flat
-              bordered
-            ></q-table>
-          </div>
-          <div class=" q-pt-xl q-pb-xl">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">First</th>
-                  <th scope="col">Last</th>
-                  <th scope="col">Another</th>
-                  <th scope="col">Handle</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(rest, key) in restaurants" :key="key">
-                  <th>{{ rest.username }}</th>
-                  <th>{{ rest.email }}</th>
-                  <th>{{ rest.googleId }}</th>
-                  <th>
-                    <QrCodeGen
-                      :value="
-                        'http://cashtag.michaelringlein.com/#/review/' +
-                          rest.googleId
-                      "
-                    >
-                    </QrCodeGen>
-                    <router-link
-                      :to="{
-                        name: 'review',
-                        params: { googleId: rest.googleId }
-                      }"
-                      exact
-                      active-class="active"
-                      >Rate this Restaurant</router-link
-                    >
-                  </th>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <div class=" q-pt-xl q-pb-xl q-pa-xl">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Restaurant Name</th>
+                <th scope="col">Restaurant Address</th>
+                <th scope="col">Google ID</th>
+                <th scope="col">Generate QR Code</th>
+                <th scope="col">Review Page Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(rest, key) in restaurants" :key="key">
+                <th>{{ rest.restaurantName }}</th>
+                <th>{{ rest.restaurantAddress }}</th>
+                <th>{{ rest.googleId }}</th>
+                <th>
+                  <a class="cursor-pointer" @click="rest.googleId = true">
+                    Generate QR Code
+                  </a>
+                </th>
+                <th>
+                  <router-link
+                    :to="{
+                      name: 'review',
+                      params: { googleId: rest.googleId }
+                    }"
+                    exact
+                    active-class="active"
+                    >Review page</router-link
+                  >
+                </th>
+                <q-dialog v-model.trim="rest.googleId">
+                  <q-card>
+                    <q-card-section>
+                      <div class="text-h6">
+                        {{ rest.restaurantName }}
+                      </div>
+                    </q-card-section>
+                    <q-card-section class="q-pt-none">
+                      <QrCodeGen
+                        :value="
+                          'http://cashtag.michaelringlein.com/#/review/' +
+                            rest.googleId
+                        "
+                      >
+                      </QrCodeGen>
+                      <p>{{ rest.restaurantName }}</p>
+                    </q-card-section>
+
+                    <q-card-actions align="right">
+                      <q-btn flat label="OK" color="primary" v-close-popup />
+                    </q-card-actions>
+                  </q-card>
+                </q-dialog>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </main>
@@ -67,156 +75,7 @@ export default {
       restaurants: null,
       restaurantsData: null,
       restaurantName: null,
-      selectedRestaurant: null,
-      columns: [
-        {
-          name: "name",
-          required: true,
-          label: "Dessert (100g serving)",
-          align: "left",
-          field: row => row.name,
-          format: val => `${val}`,
-          sortable: true
-        },
-        {
-          name: "calories",
-          align: "center",
-          label: "Calories",
-          field: "calories",
-          sortable: true
-        },
-        { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
-        { name: "carbs", label: "Carbs (g)", field: "carbs", sortable: true },
-        {
-          name: "protein",
-          label: "Protein (g)",
-          field: "protein",
-          sortable: true
-        },
-        {
-          name: "sodium",
-          label: "Sodium (mg)",
-          field: "sodium",
-          sortable: true
-        },
-        {
-          name: "calcium",
-          label: "Calcium (%)",
-          field: "calcium",
-          sortable: true,
-          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
-        },
-        {
-          name: "iron",
-          label: "Iron (%)",
-          field: "iron",
-          sortable: true,
-          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
-        }
-      ],
-
-      data: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          sodium: 87,
-          calcium: "14%",
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129,
-          calcium: "8%",
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          sodium: 337,
-          calcium: "6%",
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          sodium: 413,
-          calcium: "3%",
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          sodium: 327,
-          calcium: "7%",
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          sodium: 50,
-          calcium: "0%",
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          sodium: 38,
-          calcium: "0%",
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          sodium: 562,
-          calcium: "0%",
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          sodium: 326,
-          calcium: "2%",
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          sodium: 54,
-          calcium: "12%",
-          iron: "6%"
-        }
-      ]
+      selectedRestaurant: null
     };
   },
   methods: {
@@ -247,30 +106,22 @@ h2 {
   max-width: 734px;
   padding-top: 20px;
 }
-.my-sticky-header-table {
-  /* height or max-height is important */
-  height: 310px;
-  /* this is when the loading indicator appears */
+
+thead th {
+  font-weight: bold;
+}
+th {
+  font-weight: normal;
+  border: 1px solid;
 }
 
-.my-sticky-header-table .q-table__top,
-.my-sticky-header-table .q-table__bottom,
-.my-sticky-header-table thead tr:first-child th {
-  /* bg color is important for th; just specify one */
-  background-color: #c1f4cd;
-}
-
-.my-sticky-header-table thead tr th {
-  position: sticky;
-  z-index: 1;
-}
-
-.my-sticky-header-table thead tr:first-child th {
-  top: 0;
-}
-
-.my-sticky-header-table.q-table--loading thead tr:last-child th {
-  /* height of all previous header rows */
-  top: 48px;
+table {
+  position: relative;
+  width: 100%;
+  margin: 0;
+  line-height: 3.5;
+  border-spacing: 0;
+  border-collapse: collapse;
+  border: 1px solid;
 }
 </style>
