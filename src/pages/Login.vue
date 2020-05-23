e6916c4fd431c113f2cadee9fef8bd7cbfd701bb
Michael Ringlein <michael.ringlein@gmail.com>
2020-05-23 19:16:42 +0800

    Fixed fields in Profiles

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,58 +3,74 @@
     <main>
       <div class="bg">
         <div class="container q-pt-xl">
-          <div class="form relative-position">
-            <form @submit.prevent="loginFormSubmit">
-              <h1 class="q-pt-md">Login</h1>
-              <div class="input input1 q-mb-sm">
-                <input
-                  v-model="email"
-                  id="input1"
-                  type="text"
-                  placeholder="E - mail"
-                />
-                <label for="input1"></label>
-              </div>
-              <div class="input input2 q-mb-sm">
-                <input
-                  id="input2"
-                  type="password"
-                  v-model="password"
-                  placeholder="Password"
-                />
-                <label for="input2"></label>
-              </div>
+          <div class="column items-center">
+            <div class="col">
+              <div class="form relative-position">
+                <form @submit.prevent="loginFormSubmit">
+                  <h1 class="q-pt-md text-center">Login</h1>
+                  <div class="input input1 q-mb-sm">
+                    <q-input
+                      v-model="email"
+                      id="input1"
+                      type="text"
+                      outlined
+                      label="E-Mail *"
+                      stack-label
+                      placeholder="jemand@beispiel.com"
+                    >
+                      <template v-slot:prepend>
+                        <q-icon name="mail" />
+                      </template>
+                    </q-input>
+                  </div>
+                  <div class="input input2 q-mb-sm">
+                    <q-input
+                      id="input2"
+                      type="password"
+                      v-model="password"
+                      outlined
+                      label="Passwort *"
+                      stack-label
+                      placeholder="Passwort9754094"
+                    >
+                      <template v-slot:prepend>
+                        <q-icon name="lock" />
+                      </template>
+                    </q-input>
+                  </div>
 
-              <button class="btn full-width">Login</button>
-              <!--                <router-link-->
-              <!--                  :to="{ path: '/register' }"-->
-              <!--                  exact-->
-              <!--                  active-class="active"-->
-              <!--                  ><button onclick="prevent()" class="btn btn-secondary">-->
-              <!--                    Sign Up-->
-              <!--                  </button></router-link-->
-              <!--                >-->
-              <button
-                type="button"
-                @click="forgot = true"
-                class="btn btn-secondary full-width"
-              >
-                Passwort vergessen
-              </button>
+                  <button class="btn full-width">Login</button>
+                  <!--                <router-link-->
+                  <!--                  :to="{ path: '/register' }"-->
+                  <!--                  exact-->
+                  <!--                  active-class="active"-->
+                  <!--                  ><button onclick="prevent()" class="btn btn-secondary">-->
+                  <!--                    Sign Up-->
+                  <!--                  </button></router-link-->
+                  <!--                >-->
+                  <button
+                    type="button"
+                    @click="forgot = true"
+                    class="btn btn-secondary full-width"
+                  >
+                    Passwort vergessen
+                  </button>
 
-              <!--              <div class="forgot">-->
-              <!--                <div>-->
-              <!--                  <q-checkbox size="sm" :value="true" />-->
-              <!--                  <label>Remember me</label>-->
-              <!--                </div>-->
-              <!--              </div>-->
-              <div class="error mt-0" v-if="error">
-                {{ error }}
+                  <!--              <div class="forgot">-->
+                  <!--                <div>-->
+                  <!--                  <q-checkbox size="sm" :value="true" />-->
+                  <!--                  <label>Remember me</label>-->
+                  <!--                </div>-->
+                  <!--              </div>-->
+                  <div class="error mt-0" v-if="error">
+                    {{ error }}
+                  </div>
+                  <q-dialog v-model="forgot">
+                    <send-email />
+                  </q-dialog>
+                </form>
               </div>
-              <q-dialog v-model="forgot">
-                <send-email />
-              </q-dialog>
-            </form>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -4,6 +4,7 @@
       <div class="bg">
         <div v-if="response">
           <form class="container">
+            <h1 class="text-center">Profil</h1>
             <div class="row">
               <div class="col-lg-12 col-md-12 black-block mt-3">
                 <form
@@ -13,35 +14,52 @@
                 >
                   <div class="row q-pb-md">
                     <div class="col-md-6 q-pa-sm">
-                      <label for="mail">E-mail</label>
-                      <input
+                      <q-input
                         class="mt-1"
                         id="mail"
                         type="text"
                         :value="response.email"
-                      />
+                        outlined
+                        label="E-Mail"
+                        stack-label
+                      >
+                        <template v-slot:prepend>
+                          <q-icon name="mail" />
+                        </template>
+                      </q-input>
                     </div>
                     <div class="col-md-6 mt-2  q-pa-sm">
-                      <label for="username">Username</label>
-                      <input
+                      <q-input
                         :value="response.username"
                         class="mt-1"
                         id="username"
                         type="text"
                         disabled
-                      />
+                        outlined
+                        label="Manager Name"
+                        stack-label
+                      >
+                        <template v-slot:prepend>
+                          <q-icon name="person" />
+                        </template>
+                      </q-input>
                     </div>
                   </div>
                   <div class="row">
                     <div class="col-md-6 mt-2 q-pa-sm">
                       <div class="input input2 q-mb-md">
-                        <label for="input5">Restaurant Name</label>
-                        <input
+                        <q-input
                           id="input5"
                           type="text"
                           v-model="$v.restaurantName.$model"
-                          placeholder="Restaurant Name"
-                        />
+                          outlined
+                          label="Restaurant Name"
+                          stack-label
+                        >
+                          <template v-slot:prepend>
+                            <q-icon name="place" />
+                          </template>
+                        </q-input>
                       </div>
                       <p
                         v-if="
@@ -50,18 +68,23 @@
                         "
                         class="error m-0"
                       >
-                        Restaurant Name required
+                        Restaurant Name wird benötigt
                       </p>
                     </div>
                     <div class="col-md-6 mt-2 q-pa-sm">
                       <div class="input input2 q-mb-md">
-                        <label for="input8">Restaurant Address</label>
-                        <input
+                        <q-input
                           id="input8"
                           type="text"
                           v-model="$v.restaurantAddress.$model"
-                          placeholder="Restaurant Address"
-                        />
+                          outlined
+                          label="Restaurant Adresse"
+                          stack-label
+                        >
+                          <template v-slot:prepend>
+                            <q-icon name="place" />
+                          </template>
+                        </q-input>
                       </div>
                       <p
                         v-if="
@@ -70,37 +93,45 @@
                         "
                         class="error m-0"
                       >
-                        Restaurant Address required
+                        Restaurant Addresse wird benötigt
                       </p>
                     </div>
                     <div class="col-md-6 mt-2 q-pa-sm">
-                      <label class="mt-2 text-uppercase" for="code"
-                        >Google Places ID</label
-                      >
-                      <input
+                      <q-input
                         class="mt-1"
                         id="code"
-                        placeholder="Google Places ID"
                         type="text"
                         v-model="$v.googleId.$model"
-                      />
+                        outlined
+                        label="Google Places ID"
+                        stack-label
+                      >
+                        <template v-slot:prepend>
+                          <q-icon name="place" />
+                        </template>
+                      </q-input>
 
                       <p
                         v-if="$v.googleId.$dirty && !$v.googleId.required"
                         class="error m-0"
                       >
-                        Google Id is required
+                        Google ID wird benötigt
                       </p>
                     </div>
                     <div class="col-md-6 mt-2 q-pa-sm">
                       <div class="input input2 q-mb-md">
-                        <label for="input6">Restaurant Avatar</label>
-                        <input
+                        <q-input
                           id="input6"
                           type="text"
                           v-model="$v.restaurantAvatar.$model"
-                          placeholder="Restaurant Avatar Address"
-                        />
+                          outlined
+                          label="Restaurant Avatar"
+                          stack-label
+                        >
+                          <template v-slot:prepend>
+                            <q-icon name="place" />
+                          </template>
+                        </q-input>
                       </div>
                       <p
                         v-if="
@@ -109,7 +140,7 @@
                         "
                         class="error m-0"
                       >
-                        Restaurant Avatar required
+                        Restaurant Avatar wird benötigt
                       </p>
                       <!--                      <img :src="this.restaurantAvatar" alt="" />-->
                     </div>
@@ -121,7 +152,7 @@
                         type="button"
                         class="btn btn-primary ml-2 mb-2"
                       >
-                        Update Profile Data
+                        Profil aktualisieren
                       </button>
                       <p v-if="this.message" class="text-success">
                         {{ message }}
@@ -132,15 +163,19 @@
                 <form @submit.prevent="submitPasswordForm">
                   <div class="row q-pb-md q-pt-xl">
                     <div class="col-12 col-md-6 q-pa-sm pl-md-0 mt-2 q-pa-sm">
-                      <label class="mt-2" for="new-password"
-                        >New Password</label
-                      >
-                      <input
+                      <q-input
                         v-model="$v.passwordForm.password.$model"
                         class="mt-1"
                         id="new-password"
                         type="password"
-                      />
+                        outlined
+                        label="Passwort"
+                        stack-label
+                      >
+                        <template v-slot:prepend>
+                          <q-icon name="lock" />
+                        </template>
+                      </q-input>
                       <p
                         v-if="
                           $v.passwordForm.password.$dirty &&
@@ -148,19 +183,23 @@
                         "
                         class="error"
                       >
-                        New Password is required
+                        Neues Passwort wird benötigt
                       </p>
                     </div>
                     <div class="col-12 col-md-6 q-pa-sm pl-md-0 mt-2 q-pa-sm">
-                      <label class="mt-2" for="passwordConfirmation"
-                        >New Password Confirmation</label
-                      >
-                      <input
+                      <q-input
                         v-model="$v.passwordForm.passwordConfirmation.$model"
                         class="mt-1"
                         id="passwordConfirmation"
                         type="password"
-                      />
+                        outlined
+                        label="Passwort"
+                        stack-label
+                      >
+                        <template v-slot:prepend>
+                          <q-icon name="lock" />
+                        </template>
+                      </q-input>
                       <p
                         v-if="
                           $v.passwordForm.passwordConfirmation.$dirty &&
@@ -168,7 +207,7 @@
                         "
                         class="error"
                       >
-                        Password Confirmation is required
+                        Passwort bestätigen ist erforderlich
                       </p>
                       <p
                         v-if="
@@ -176,14 +215,14 @@
                         "
                         class="error"
                       >
-                        Passwords must be identical
+                        Passwörter müssen übereinstimmen
                       </p>
                     </div>
                     <div class="col-md-12 q-pa-sm pl-md-0 mt-2 q-pa-sm">
                       <input
                         class="btn btn-primary mt-3"
                         type="submit"
-                        value="Update Password"
+                        value="Passwort aktualisieren"
                       />
                       <p class="text-success ml-3" v-if="passwordMessage">
                         {{ passwordMessage }}
